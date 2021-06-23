0c65e06971bf52f04803deda76f53008a3db2031
Tanishq Gupta <tanishq67@gmail.com>
2021-06-24 00:59:20 +0530

    updated requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'gffutils=0.9', 'pybedtools']
 
 test_requirements = [ ]
 
